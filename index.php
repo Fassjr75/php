c27784833c393990c5daf78258749ec3790df9ca
= <=>
2021-11-24 13:16:06 -0300

    Mudei o primeiro echo e acrescentei um segundo localmente

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php 
 
-	echo "Aprendendo a usar o GIT - E agora fiz uma alteração nele";
+	echo "Aprendendo a usar o GIT - E agora fiz uma alteração nele. Fazendo dar erro";
+
+
+
+
+
+	echo "Agora vai dar conflito porque este arquivo foi alterado localmente e também remotamente."
 
 	
